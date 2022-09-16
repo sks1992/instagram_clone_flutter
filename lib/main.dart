f213134fb9fa59d22596db03c63f85efbb48c845
Sandeep <sandeepsk15892>
2022-09-16 14:54:03 +0530

    firebase setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
@@ -7,7 +8,20 @@ import 'package:instagram_clone/utils/colors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyDHPBrH1BQ7d5b133XpRhdVvlBEbtW3diQ",
+        appId: "1:705169374184:web:eed8bcefd9ad92a695825d",
+        messagingSenderId: "705169374184",
+        projectId: "instagramclone-6b49c",
+        storageBucket: "instagramclone-6b49c.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
